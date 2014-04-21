2794df73043f318fbc1327d15398fe4efbb165d7
Hung Quoc Ly <hung.ly.q@gmail.com>
2014-04-21 10:58:08 +0700

    Before adding AI

---
+++ levels/aaa.pac
@@ -0,0 +1,15 @@
+Anonymous
+Unknown
+10
+30
+ddddccxcczcwwwwwwwwwwwwwwwwwwe
+a                            d
+a         c                  d
+a         x                  d
+a         c                  d
+a         x                  d
+a         c                  d
+a          cxzc              d
+a                            d
+zxxxxxxxxxxxxxxxxxxxxxxxxxxxxc
+
--- src/pacman.c
+++ src/pacman.c
@@ -11,137 +11,75 @@
 #include <sys/ioctl.h>
 #include <time.h>
 #include <unistd.h>
-
 /** Define the directory when opening using sh file in root folder of project*/
 #define DIRECTORY_SH            "levels/"
-
 /** Define the directory when opening using the executable file in source folder*/
 #define DIRECTORY_SRC           "../levels/"
-
 /** Define ColorPallette1 for terminal background */
 #define COLOR_BACKGROUND        0
-
 /** Define ColorPallette2 for map background */
 #define COLOR_MAP_BACKGROUND    1
-
 /** Define ColorPallette3 for error message */
 #define COLOR_ERROR_BACKGROUND  2
-
 /** Define ColorPallette4 for letters */
 #define COLOR_LETTER            3
-
 /** Define ColorPallette5 for wall in map */
 #define COLOR_WALL              4
-
 /** Define ColorPallette6 for pacman */
 #define COLOR_PACMAN            5
-
 /** Define ColorPallette7 for fruit */
 #define COLOR_FRUIT             6
-
 /** Define ColorPallette8 for ghost */
 #define COLOR_GHOST             7
-
 /** Pointer to array to char which indicates the author of current map */
 char *author;
-
 /** Pointer to array to char which indicates the present working directory */
 char *directory;
-
 /** Pointer to array of char which indicates the list of availble levels */
 char * levels[] = {"level2.pac", "level3.pac", "level1.pac"};
-
 /** Pointer to array to char which indicates the file name of current map */
 char *file_name;
-
 /** Pointer to array of char which indicates the current map */
 char *map;
-
 /** Pointer to array to char which indicates the title of current map */
 char *title;
-
 int atePellet = 0;
-
 int end_game = 0;
-
 /** An integer value which indicate the program is ended or not */
 int end_program = 0;
-
 /** An integer value which indicate the number of error message */
 int error_msg_count = 1;
-
 /** An integer value which indicate the number of maps availble */
 int num_levels = 3;
-
 int live = 3;
-
 int level = 1;
-
 /** An integer value which indicates the height of map */
 int height;
-
 /** An integer value which indicates the width of map */
 int width;
-
 /** An integer value which indicates the row where the cursor is located in the map */
 int x = 0;
-
 /** An integer value which indicates the column where the cursor is located in the map */
 int y = 0;
-
 /** An integer value which indicates the distance (in rows) between the top of terminal and the first row of the map */
 int x_offset;
-
 /** An integer value which indicates the distance (in columns) between the left most of terminal and the first column of the map */
 int y_offset;
-
 /** reti is an integer which store the result of regex comparision */
 int reti;
-
 int score = 0;
-
 int totalPellet;
-
 /** regex is used to  check for input pattern */
 regex_t regex;
-
 struct pacghost pacman;
-
 struct pacghost ghosts[4];
-
 struct pacghost * ghost = &(ghosts[0]);
-
 /** The size of ncruses terminal in characters of height and width */
 struct winsize w;
-
+void showMenu(int argc, char *argv[]);
+void startNewGame(int argc, char *argv[]);
 int main(int argc, char *argv[]) {
-	int input;
-	int count = 0;
-
-	struct timespec delay = {0, 250000000L}, 
-                     rem;
-
-	/* determine the current working directory and the levels folder for save map */
-    if (startsWith("./src/", argv[0])) {
-        directory = DIRECTORY_SH;
-    } else {
-        directory = DIRECTORY_SRC;
-    }
-    
-    /* initialise ncurses screen */
-    initscr();
-
-    /* enable the use of function keys, allow navigating the cursor using arrow keys */
-    keypad(stdscr, TRUE);
-
-    /* disable echo when getch */
-    noecho();
- 
-    /* take input chars, does not wait until new line or carriage return */
-    cbreak();
-
-    timeout(0);
-
+	initscr();
     /* set color pairs */
     if(has_colors()) {
         start_color();
@@ -155,12 +93,56 @@ int main(int argc, char *argv[]) {
         init_pair(8, COLOR_PACMAN,	COLOR_BACKGROUND);
         init_pair(9, COLOR_GHOST,	COLOR_BACKGROUND);
     }
+	showMenu(argc, argv);
+    endwin();
+    exit(0);
+}
 
+void showMenu(int argc, char *argv[]){
+	while(1){
+		clear();
+		move(0,0);
+		printw("1. Start new game\n2. Edit mode\n3. High score\n4. Game credit\n5. Quit\n");
+		printw("Press 1,2,3,4 to start function, other to quit program\n");
+		//	timeout(-1);
+		int choice = getch();
+		if(choice == '1'){
+			startNewGame(argc, argv);
+		} else if (choice == '2'){
+			editor(argc, argv);
+		} else if (choice == '3'){
+			//High score
+			printw("high score");
+		} else if(choice == '4'){
+			printw("Game credit");
+		} else {
+			endwin();
+			return;
+		}
+	}
+}
+
+void startNewGame(int argc, char *argv[]){
+	int input;
+	int count = 0;
+	struct timespec delay = {0, 250000000L}, 
+                     rem;
+	/* determine the current working directory and the levels folder for save map */
+    if (startsWith("./src/", argv[0])) {
+        directory = DIRECTORY_SH;
+    } else {
+        directory = DIRECTORY_SRC;
+    }
+    /* enable the use of function keys, allow navigating the cursor using arrow keys */
+    keypad(stdscr, TRUE);
+    /* disable echo when getch */
+    noecho();
+    /* take input chars, does not wait until new line or carriage return */
+    cbreak();
+    timeout(0);
     /* get terminal size */
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-
     while(!end_game) {
-
     	mvprintw(w.ws_row - 1, w.ws_col - 1, "%d", levels[(level - 1) % num_levels]);
     	read_file(levels[(level - 1) % num_levels]);
 
@@ -171,19 +153,13 @@ int main(int argc, char *argv[]) {
 		mvprintw(w.ws_row - 1, 0, "Live:  ");
 		mvprintw(w.ws_row - 2, w.ws_col / 4 * 3 + 7, "Level: ");
 		attrset(COLOR_PAIR(1));
-
     	search_pacman(map, &pacman);
     	search_ghost(map, ghost);
-
     	atePellet = 0;
-
     	count_pellet(map, &totalPellet);
-
     	pacman.direction = 1;
-
 	    while (!end_game) {
 	    	input = getch();
-
 	        if (input == 'q' || input == 'Q')
 	        {
 	            end_game = 1;
@@ -221,7 +197,6 @@ int main(int argc, char *argv[]) {
 	                    break;
 	            }
 	        }
-
 	        if (isWin(atePellet, totalPellet)) {
 	            mvprintw (0, 0, "%d,%d,%d", atePellet, totalPellet, level);
 	            level++;
@@ -229,15 +204,11 @@ int main(int argc, char *argv[]) {
 	            //getch();
 	            break;
 	        }
-
 	        update_score(map, height, width, &pacman, &score, &atePellet);
-
 	        mvprintw(w.ws_row - 1, w.ws_col - 10, "%d", count);
 	        mvprintw(w.ws_row - 1, w.ws_col - 30, "%5d,%5d", atePellet, totalPellet);
-
 	        delete_characters(&pacman, ghost);
 	        move_character(&pacman);
-
 	        for (int i = 0; i < 4; i++) {
 	            if (isCollision(&pacman, &ghosts[i])) {
 	                if (live <= 1) {
@@ -249,32 +220,21 @@ int main(int argc, char *argv[]) {
 	                }
 	            }
 	        }
-
 	        display_score();
 	        display_characters(&pacman, ghost);  
-
 	        nanosleep(&delay, &rem);
 	    }
 	}
-
 	if (map) {
 		free(map);
 	}
-
 	if (author) {
 		free(author);
 	}
-    
     if (title) {
     	free(title);
     }
-    
     if (file_name) {
     	free(file_name);
     }
-
-    endwin();
-    exit(0);
-
-	// editor(argc, argv);
 }
